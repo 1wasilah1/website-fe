336cc70415248d59e1299882d4cb02a4824ac08d
mutiaayuriz <Marizky2008@gmail.com>
2025-04-28 13:17:10 +0700

    remove unused code

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,9 +5,6 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 font-[inherit]">
           {/* Informasi Perusahaan */}
           <div>
-            <div className="w-full border border-solid">
-            mapsny
-            </div>
             <h2 className="text-lg font-semibold">Alamat Utama</h2>
             <p className="mt-2 text-sm">
               Jl. Taman Jatibaru No.1, RT.17/RW.1, Kelurahan Cideng, Kecamatan Gambir, Kota Jakarta Pusat, 
